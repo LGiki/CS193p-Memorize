e77aac5a9c9d8dc7281c8cc04dd90532cf9923e1
LGiki <qq547221432@gmail.com>
2025-01-24 22:37:14 +0800

    Initial Commit

---
+++ cs193p_Memorize/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  cs193p_Memorize
+//
+//  Created by LGiki on 2025/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ cs193p_Memorize/cs193p_MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  cs193p_MemorizeApp.swift
+//  cs193p_Memorize
+//
+//  Created by LGiki on 2025/1/24.
+//
+
+import SwiftUI
+
+@main
+struct cs193p_MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
